a7f63cadc2cba24b195d53f1c3e6f15f6f361f0e
koenvs <koen.vansever@gmail.com>
2020-10-14 00:27:19 +0200

    update 14102020

--- koenvs_home/apps/serial/views.py
+++ koenvs_home/apps/serial/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 from static.my_modules.safeserial import SafeSerial as Sfs  # pylint: disable=import-error
-from json import dumps, dump
+from json import dumps, dump, loads
 import serial.tools.list_ports as stl  # pylint: disable=import-error disable=no-name-in-module
-
-# * HELP FUNCTIONS
+from time import sleep
 
 
 def gen_ports_list():
@@ -16,21 +15,124 @@ def gen_ports_list():
             f"{ports_list[i].device}  -  {ports_list[i].description}")
     return ports_list, ser_ports
 
-# * DATA OBJECTS FOR START
 
+# * DATA OBJECTS FOR START
+# ! THESE ARE GLOBAL VARIABLES, TAKE CARE WITH GLOBAL SCOPE WITHIN FUNCIONS
 
 _, ports_list = gen_ports_list()
-SER = Sfs()  # /i MAIN SER OBJECT
-entry_data = {"arg1": 5, "arg2": 3, "conv": "HPC", "sel_port": ports_list[0]}
-print(ports_list)
-with open("temporary/test.json", "w") as f:
-    dump({"entry_data": dumps(entry_data), "ser_ports": ports_list}, f)
+ser = Sfs()  # /i MAIN SER OBJECT
+ser.close()  # /i close port by default when starting application
+entry_data = {"arg1": 5, "arg2": 3, "conv": "HPC",
+              "sel_port": ports_list[0], "command": ""}
+terminal_text = [""]
+prefix = ""
+# with open("temporary/test.json", "w") as f:
+#     dump({"entry_data": dumps(entry_data), "ser_ports": ports_list}, f)
+
 
 # * Create your views here.
 
+def on_ser_closed(request, comm_proc, sel_port):
+    """ Does actions when port is closed upon button click """
+    global prefix
+    if comm_proc == "open":
+        ser.close()
+        ser.port = sel_port
+        try:
+            ser.open()
+        except Exception:
+            terminal_text.append("Selected port invalid")
+            return render_ser_index(request)
+        read = ser.open_admin()
+        if "(3)" in read:
+            prefix = read.split("\n")[1]
+            terminal_text.append("Terminal opened on correct level!")
+        else:
+            terminal_text.append("Something went wrong!")
+    elif comm_proc == "exit":
+        terminal_text.append("Port is already closed")
+    else:
+        terminal_text.append(
+            "Please open the terminal with the correct port selected")
+
+
+def on_ser_open_open():
+    """Does the actions of the serial port and terminal printing when serial port is open"""
+    ser.write_reg("open")
+    sleep(0.05)
+    ret = ser.read_print_buffer()
+    if "(3)" in ret:
+        terminal_text.append(
+            "Terminal opened on correct level!")
+    else:
+        i = 0
+        while i < 2:
+            ret = ser.open_admin()
+            if "(3)" in ret:
+                prefix = ret.split("\n")[1]
+                terminal_text.append(
+                    "Terminal opened on correct level!")
+                break
+            i += 1
+
+
+def append_resp(command, wait=0.05):
+    """ Sends command and changes resp to page """
+    global terminal_text
+    ser.write_reg(command)
+    sleep(wait)
+    buf = ser.read_print_buffer().split("\n")
+    terminal_text.append(prefix + buf[0].rstrip())
+    n = len(buf)
+    terminal_text = terminal_text + buf[1:n-1]
+    return None
+
+
+def render_ser_index(request):
+    global entry_data
+    global terminal_text
+    global ports_list
+    entry_data["command"] = ""
+    return render(request, "serial/index.html", {"entry_data": dumps(entry_data), "ser_ports": ports_list, "terminal_text": terminal_text})
+
 
 def serialIndex(request):
+    global entry_data
+    global terminal_text
     if request.method == "GET":
-        return render(request, "serial/index.html", {"entry_data": dumps(entry_data), "ser_ports": ports_list})
+        entry_data["command"] = ""
+        terminal_text = [""]
+        return render_ser_index(request)
     elif request.method == "POST":
-        return HttpResponse(request, "This is a post request")
+
+        entry_data = loads(request.POST["data"])
+        print(entry_data)
+        comm_proc = entry_data["command"]
+        sel_port = entry_data["sel_port"].split("-")[0].rstrip()
+        # print(comm_proc)
+
+        if not ser.is_open:
+            on_ser_closed(request, comm_proc, sel_port)
+
+        elif ser.is_open:
+            if ser.port == sel_port:
+                if comm_proc == "open":
+                    on_ser_open_open()
+                elif comm_proc == "clear":
+                    terminal_text = [""]
+                elif comm_proc == "exit":
+                    # ser.write_reg("exit")
+                    # sleep(0.05)
+                    # ret = ser.read_print_buffer()
+                    ser.close()
+                    terminal_text.append("Port closed")
+                elif comm_proc == "getver":
+                    append_resp("getver")
+                elif comm_proc == "readvolt":
+                    append_resp("readvolt")
+            else:
+                terminal_text.append(
+                    "Opened port and selected port are different, please specify right port!")
+
+        # print(terminal_text)
+        return render_ser_index(request)
--- koenvs_home/static/js/serial.js
+++ koenvs_home/static/js/serial.js
@@ -1,26 +1,79 @@
+// * Declaration of variables
+let arg1 = document.querySelector("#entry_arg1")
+let arg2 = document.querySelector("#entry_arg2")
+let hpc = document.querySelectorAll("input[type='radio'][value='HPC']")[0]
+let fo = document.querySelectorAll("input[type='radio'][value='FO/LCC']")[0]
+let portList = document.querySelectorAll("option[class='conv_list']")
+let finalForm = document.getElementById("finalForm");
+let finalData = document.getElementById("finalData");
+
+// /i function declaration: loads before any code is executed (delays program before any code is ran)
+// /i this also means that the function can be called before it is declared!!! function expression can not do that
 function printSomething(data) {
     console.log(data);
+};
+// /i function declaration: loads before any code is executed (delays program before any code is ran)
+// /i this also means that the function can be called before it is declared!!! function expression can not do that
+function getConvStatus() {
+    if (hpc.checked) {
+        return "HPC";
+    } else if (fo.checked) {
+        return "FO/LCC";
+    } else {
+        return "Unknown";
+    };
+};
+
+// /i function expression: are loaded when encountered (won't slow down your code if not called)
+// /i can't be declared after usage
+// /i handy if used in a conditional block (you are not loading when you don't need that shit)
+let getPortStatus = () => {
+    let temp = document.querySelectorAll("option.conv_list");
+    let res;
+    for (x in temp) {
+        if (temp[x].selected) {
+            res = temp[x].value;
+        };
+    };
+    return res;
 }
 
 function updateEntryData(entry) {
-    document.querySelector("#entry_arg1").value = entry["arg1"];
-    document.querySelector("#entry_arg2").value = entry["arg2"];
+    arg1.value = entry["arg1"];
+    arg2.value = entry["arg2"];
     if (entry["conv"] == "HPC") {
-        document.querySelectorAll("input[type='radio'][value='HPC']")[0].setAttribute("checked", "checked")
+        hpc.setAttribute("checked", "checked")
     } else {
-        document.querySelectorAll("input[type='radio'][value='FO/LCC']")[0].setAttribute("checked", "checked")
+        fo.setAttribute("checked", "checked")
     };
-    let tempList = document.querySelectorAll("option[class='conv_list']");
-    for (i = 0; i < tempList.length; i++) {
-        tempList[i].removeAttribute("selected") // TODO: figure out the visibility issue!
-        // console.log("infor")
-        if (tempList[i].value == entry["sel_port"]) {
-            // console.log("inif")
-            tempList[i].setAttribute("selected", "selected")
+    portList = document.querySelectorAll("option[class='conv_list']"); // ! not sure if necessary to refresh
+    for (i = 0; i < portList.length; i++) {
+        portList[i].removeAttribute("selected") // TODO: figure out the visibility issue!
+        // console.log("infor");
+        if (portList[i].value == entry["sel_port"]) {
+            // console.log("inif");
+            portList[i].setAttribute("selected", "selected");
         }
     };
+
     console.log(entry);
 };
+
+function getEntryData(comm) {
+    let temp = {
+        "arg1": arg1.value,
+        "arg2": arg2.value,
+        "conv": getConvStatus(),
+        "sel_port": getPortStatus(),
+        "command": comm,
+    };
+    return JSON.stringify(temp)
+};
+
+function sendData(comm) {
+    finalData.setAttribute("value", getEntryData(comm))
+    finalForm.submit();
+}
 
 // ACTIONS ON LOADING
 updateEntryData(ent)
